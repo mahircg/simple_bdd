5ad087e7b23682ffd6ff163ed83585c8611ad6d1
Mahircan Gul <guelm@rhrk.uni-kl.de>
2015-02-24 01:44:12 +0100

    Simple BDD_ID header and implementation

---
+++ bdd_package/src/BDD_ID.cpp
@@ -0,0 +1,56 @@
+#include "BDD_ID.hpp"
+
+
+
+Node::operator std::string() const
+{
+	return to_string(static_cast<long long>(id));
+}
+
+void Node::setID(unsigned id)
+{
+	this->id = id;
+}
+
+unsigned Node::getID()
+{
+	return id;
+}
+
+
+
+ostream& operator<<(ostream& osObject,const BDD_ID &bddObj)
+{
+	osObject<<bddObj.name<<" -> "<<static_cast<string>(*(bddObj.low))<<"|"<<static_cast<string>(*(bddObj.high));
+	return osObject;
+}
+
+
+
+Node::Node(unsigned id):id(id){};
+
+
+BDD_ID::BDD_ID(string name,unsigned id):Node(id), name(name) {};
+
+BDD_ID::BDD_ID(string name,unsigned id,Node* t,Node* e):Node(id), name(name) 
+{
+	low=e;
+	high=t;
+}
+
+
+BDD_ID::BDD_ID(string name,unsigned id,Node& t,Node& e):Node(id), name(name) 
+{
+	low=&e;
+	high=&t;
+}
+
+void BDD_ID::setLow(class Node& e)
+{
+	low=&e;
+}
+
+void BDD_ID::setHigh(class Node& t)
+{
+	high=&t;
+}
--- bdd_package/src/BDD_ID.hpp
+++ bdd_package/src/BDD_ID.hpp
@@ -1,11 +1,49 @@
 #ifndef HPP_BDD_ID
 #define HPP_BDD_ID
 
-#include<string.>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
-class BDD_ID
+
+
+
+/**
+ * \class Node 
+ * \brief Base class for DAG node
+ * 
+ * A node might be either a variable or a constant(such as terminal or leaf node)
+ */
+class Node
 {
+protected:
+	unsigned id;
+public:
+	Node(unsigned);	
+	void setID(unsigned);
+	unsigned getID();
+	virtual operator string() const;
+};
+
+
+/**
+ * \class BDD_ID 
+ * \brief This class is used for storing the BDD structure
+ * 
+ * Each node(except leaves) has its successors which are either variables or terminal nodes.
+ */
+class BDD_ID : public Node
+{
+	friend ostream& operator<<(ostream&,const BDD_ID&);
+private:
+	class Node *low,*high;
+	string name;
+public:
+	BDD_ID(string,unsigned);
+	BDD_ID(string,unsigned,Node*,Node*);
+	BDD_ID(string,unsigned,Node&,Node&);
+	void setLow(Node&);
+	void setHigh(Node&);
 };
 #endif
