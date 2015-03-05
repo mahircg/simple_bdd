ee1c5c51880728cdde5b6dd1f556368815afd892
nt88 <nicoale.tusinschi@gmail.com>
2015-03-05 13:53:13 +0100

    Basic Implementation of ITE, CoFactor. Partial Tests Written

--- bdd_package/src/Manager.cpp
+++ bdd_package/src/Manager.cpp
@@ -48,139 +48,155 @@ bool Manager::isConstant(const BDD_ID& node)
 
 size_t Manager::topVar(const BDD_ID& node) // Root Node Id
 {
-	return node.getID();
+	return (unsigned)node;
 }
 
-BDD_ID Manager::coFactorTrue(const BDD_ID f,const BDD_ID g)
+BDD_ID Manager::coFactorTrue(const BDD_ID f,const BDD_ID x)
 {
-	if (f.getID()==1 || g.getID()==1){
-		return 	False();
+	// Base Case
+	if (f.getID() < x.getID()){
+		return x;	
 	}
-	if (f.getID()==2 || g.getID()==2){
-		return 	f;
+	else if (isConstant(f)){
+		return f;
 	}
-	if (coFactorFalse(g) == True()){
-		return coFactorTrue(coFactorTrue(f),coFactorTrue(g));	
+	else if (f.getID() == x.getID()){
+		return *f.getHigh;
 	}
-	if (coFactorTrue(g) == False()){
-		return coFactorTrue(coFactorFalse(f),coFactorFalse(g));	
+	// Recursive Case
+	else{
+		BDD_ID RHH = coFactorTrue(*f.getHigh(),x);
+		BDD_ID RLL = coFactorTrue(*f.getLow(),x);
+		if (RHH == RLL){
+			return RHH;		
+		}
+		nextID += 1;
+		BDD_ID tmp((string)x,nextID,RHH,RLL);
+		pair<BDD_ID,unsigned> elem(tmp,nextID);
+		//it = uniqueTable.find(elem);
+		//if (it == 0){
+		//	uniqueTable.insert(elem);
+		//}
+		return tmp;	
 	}
-	BDD_ID t = coFactorTrue(coFactorTrue(f),coFactorTrue(g));
-	BDD_ID e = coFactorTrue(coFactorFalse(f),coFactorFalse(g));
-	if (t == e){
-		return t;	
-	}
-	/* TODO: Find Or Create Node to be Implemented -> Unique Table*/
-	
 }
 
-BDD_ID Manager::coFactorFalse(const BDD_ID f,const BDD_ID g)
+BDD_ID Manager::coFactorFalse(const BDD_ID f,const BDD_ID x)
 {
-	if (f.getID()==1 || g.getID()==1){
-		return 	f;
+	// Base Case
+	if (f.getID() < x.getID()){
+		return x;	
 	}
-	if (f.getID()==2 || g.getID()==2){
-		return 	True();
+	else if (isConstant(f)){
+		return f;
 	}
-	if (coFactorFalse(g) == True()){
-		return coFactorFalse(coFactorTrue(f),coFactorTrue(g));	
+	else if (f.getID() == x.getID()){
+		return *f.getLow;
 	}
-	if (coFactorTrue(g) == False()){
-		return coFactorFalse(coFactorFalse(f),coFactorFalse(g));	
+	// Recursive Case
+	else{
+		BDD_ID RHH = coFactorFalse(*f.getHigh(),x);
+		BDD_ID RLL = coFactorFalse(*f.getLow(),x);
+		if (RHH == RLL){
+			return RLL;		
+		}
+		nextID += 1;
+		BDD_ID tmp((string)x,nextID,RHH,RLL);
+		pair<BDD_ID,unsigned> elem(tmp,nextID);
+		//it = uniqueTable.find(elem);
+		//if (it == 0){
+		//	uniqueTable.insert(elem);
+		//}
+		return tmp;	
 	}
-	BDD_ID t = coFactorFalse(coFactorTrue(f),coFactorTrue(g));
-	BDD_ID e = coFactorFalse(coFactorFalse(f),coFactorFalse(g));
-	if (t == e){
-		return t;
-		
-	}
-	/* TODO: Find Or Create Node to be Implemented -> Unique Table*/
 }
 
 BDD_ID Manager::coFactorTrue(const BDD_ID f) // T CoFactor of f
 {
-	if (f.getID()==1){
-		return False();
-	}
-
-	else if ((f.getLow())->getID() == (f.getHigh())->getID() == f.getID()){
+	if (isConstant(f)){
 		return f;
 	}
-	else if (f.getID()==2){
+	
+	if ((f.getLow())->getID() == (f.getHigh())->getID() == f.getID()){
 		return f;
 	}
-	// return *f.getHigh();
+	return *f.getHigh();
 }
 
 BDD_ID Manager::coFactorFalse(const BDD_ID f) // E CoFactor of f
 {
-	if (f.getID()==2){
-		return True();
-	}
-	else if ((f.getLow())->getID() == (f.getHigh())->getID() == f.getID()){
+	if (isConstant(f)){
 		return f;
 	}
-	else if (f.getID()==1){
+	
+	if ((f.getLow())->getID() == (f.getHigh())->getID() == f.getID()){
 		return f;
 	}
-	// return *f.getLow();
+	return *f.getLow();
+
 }
 
 BDD_ID Manager::ite(const BDD_ID f,const BDD_ID g,const BDD_ID h)
-{
-    	/*===== Terminal Case Recursion Check =====*/
-	if ((f.getID()==2)||(f.getID()==1)||((g.getID()==2) && (h.getID()==1))||(g.getID()==h.getID())){
+{  	
+	// Terminal Cases of Recursion
+	if (f==*True()){
+		return g;	
+	}
+	else if (f==*False()){
+		return h;
+	}
+	else if ((g==*True())&&(h==*False())){
+		return f;
+	}
+	else if (g==h){
 		return f;	
 	}
-	else if ((g.getID()==1) && (h.getID()==2)){
-		return f;	
+	else if ((g==*False())&&(h==*True())){
+		return neg(f);
 	}
-	/*TODO: Check Computed Table: Eliminate Repetitions in Computations */
-	
-	else{
-		BDD_ID rhh = ite(*f.getHigh(), *g.getHigh(), *h.getHigh());
-		BDD_ID rll = ite(*f.getLow(), *g.getLow(), *h.getLow());
-		if (rhh == rll){
-			return rhh;		
-		}
-		/*TODO: Find Or Add in/to Unique Table -> Eliminate Isomorphic Sub-Graphs */
-		/* BDD_ID r = find_or_add_unique_table(topVar, rhh, rll) 
-		END TODO*/
-		/*TODO: Update Computed Table 
-		END TODO*/
-		/* return r;*/
+	else {
+		BDD_ID x=topVar(f);
+      		BDD_ID t=ite(coFactorTrue(f,x),coFactorTrue(g,x),coFactorTrue(h,x));
+      		BDD_ID e=ite(coFactorFalse(f,x),coFactorFalse(g,x),coFactorFalse(h,x));
+      		if(t==e)
+			return t;
+      		nextID += 1;
+      		BDD_ID tmp((string)x,nextID,t,e);
+      		pair<BDD_ID,unsigned> elem(tmp,nextID);
+      		uniqueTable.insert(elem);
+      		return tmp;	
 	}
 
 }
 
 BDD_ID Manager::and2(const BDD_ID f,const BDD_ID g)
 {
-        return BDD_ID(" ",1);
+  return ite(f,g,*False());
 }
 
 BDD_ID Manager::or2(const BDD_ID f,const BDD_ID g)
 {
-        return BDD_ID(" ",1);
+  return ite(f,*True(),g);
 }
 
 BDD_ID Manager::xor2(const BDD_ID f,const BDD_ID g)
 {
-       return BDD_ID(" ",1);
+  return ite(f,neg(g),g);
 }
 
 BDD_ID Manager::nand2(const BDD_ID f,const BDD_ID g)
 {
-	return BDD_ID(" ",1);
+  return ite(f,neg(g),*True());
 }
 
 BDD_ID Manager::nor2(const BDD_ID f,const BDD_ID g)
 {
-        return BDD_ID(" ",1);
+  return ite(f,*False(),neg(g));
 }
 
 BDD_ID Manager::neg(const BDD_ID f)
 {
-	return BDD_ID(" ",1);
+  return ite(f,*False(),*True());
 }
 
 string Manager::getTopVarName(const BDD_ID& f)
@@ -196,4 +212,3 @@ void Manager::findNodes(const BDD_ID& f,set<BDD_ID>& list)
 void Manager::findVars(const BDD_ID& f,set<BDD_ID>& list)
 {
 }
-
